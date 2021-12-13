4e0a6f9d6e1a4c001f4ab8b060ee9eebff0074ac
JuniperRhy <Gwyniver.Rhy@gmail.com>
2021-12-13 11:50:11 -0600

    fix axios

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -91,69 +91,8 @@ export default function NewsFeed({ isBDClicked, changeFont }) {
                 </p>
               </li>
             ))}
-          {/* <li>
-            <h1
-              style={{
-                webkitTextStrokeColor: changeFont
-                  ? "transparent"
-                  : "rgb(219, 190, 24)",
-              }}
-            >
-              Star Wars News
-            </h1>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Quibusdam, non labore. Culpa ullam expedita, illo iusto suscipit
-              ut molestiae recusandae cupiditate, magnam dolorem quis at quaerat
-              et nulla fugit modi harum eos ex est, veniam animi sapiente
-              numquam. Libero dolorum quisquam ut? Beatae porro officiis quae
-              consequuntur laboriosam repellat voluptatibus!
-            </p>
-          </li>
-          <br />
-          <li>
-            <h1
-              style={{
-                webkitTextStrokeColor: changeFont
-                  ? "transparent"
-                  : "rgb(219, 190, 24)",
-              }}
-            >
-              Star Wars News
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              optio facere libero iusto quo itaque dicta doloremque quaerat
-              deserunt unde. Corporis facere saepe praesentium debitis sit
-              laudantium quae inventore cumque, eum facilis iusto delectus
-              ipsum, ipsam quisquam provident accusantium architecto! Tempora
-              harum odit possimus asperiores eligendi dignissimos distinctio
-              perferendis? Quae, molestias non. Illo itaque iusto maxime dolorum
-              temporibus blanditiis asperiores quia, excepturi laudantium, dolor
-              quam omnis, dolorem vel quod voluptate velit veritatis similique
-              maiores sequi ratione perferendis. Tempora, numquam architecto?
-            </p>
-          </li> */}
         </ul>
       </div>
     </div>
   );
 }
-
-// console.log(response.data);
-
-/*
-      
-      function filter<cheerio.Element, cheerio.Element>(this: cheerio.Cheerio<...>, match: (index: number, value: cheerio.Element) => value is cheerio.Element): cheerio.Cheerio<...> (+1 overload)
-
-      Now we see why (h2) => h2 is a number
-
-      (index, h2) => boom
-
-      (method) Array<string>.filter<S>(predicate: (value: string, index: number, array: string[]) => value is S, thisArg?: any): S[] (+1 overload)
-
-      You can see in the standard filter the first parameter given to the callback is the value (the current element), then the index of it, then the whole array itself.
-
-
-
-      */ // <--- won't do any normal filter stuff, will just run our custom function and print 'I do what I want...'
